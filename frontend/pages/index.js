f5bc32293ca4ce69cdd28fb665bdb482aa9cc082
Alice Freda <alicefreda@gmail.com>
2021-03-31 10:21:32 -0700

    add pages

---
+++ frontend/pages/index.js
@@ -0,0 +1,7 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <p>I am the home page</p>
+    </div>
+  );
+}
---
+++ frontend/pages/products.js
@@ -0,0 +1,7 @@
+export default function ProductPage() {
+  return (
+    <div>
+      <p>I am the products page</p>
+    </div>
+  );
+}
